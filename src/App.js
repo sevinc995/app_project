82edd8ef52887e5515e761d3dc50e70b30b64d97
sevinc995 <sevincseferova25@gmail.com>
2025-08-27 08:46:06 +0000

    change

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import { useState } from "react";
 
+
+function Greeting () {
+  const [pupil, setPupil] = useState(" ");
+  
+  return (
+    <>
+      <h1>Salam, mən {pupil}</h1>
+      <button type="button" onClick={() => setPupil("Sevincəm")}>0</button>
+      <button type="button" onClick={() => setPupil("əlaçıyam")}>1</button>
+      <button type="button" onClick={() => setPupil("yaxşi oxuyanam")}>2</button>
+      <button type="button" onClick={() => setPupil("orta oxuyanam")}>3</button>
+    </>
+  )
+    
+  
+}
+
+export default Greeting;
